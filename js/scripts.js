7f23d10ffdc0f0a7116139bb4e75e892855aa0b5
Alex Lee and Nathaniel Meyer <alex@>
2017-02-13 10:22:05 -0800

    Add BMI and temp conversion in comments.

--- js/scripts.js
+++ js/scripts.js
@@ -22,3 +22,27 @@ var number2 = parseInt(prompt("Enter another number:"));
 //alert(subtract(number1, number2));
 //alert(multiply(number1, number2));
 alert(divide(number1, number2));
+
+/*
+var weight = parseInt(prompt("What is your weight in kilograms?"));
+var height = parseInt(prompt("What is your height in meters?"));
+var calculateBMI = function(weight,height){
+	return(weight / height / height);
+};
+alert("Your BMI is " + calculateBMI(weight,height) + ". Go to the hospital."); */
+
+
+/*
+var fahrenheit = parseInt(prompt("Enter fahrenheit: "));
+var celsius = parseInt(prompt("Enter celcius: "));
+
+var celsiusToFahrenheit = function(cel){
+	return (cel*(9/5)) +32;
+}
+
+var fahrenheitToCelsius = function(fah){
+	return (fah-32) * (5/9);
+};
+
+alert("Celsius to fahrenheit is: " + celsiusToFahrenheit(celsius));
+alert("Fahrenheit to celsius is: " + fahrenheitToCelsius(fahrenheit)); */
