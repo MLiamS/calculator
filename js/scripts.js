fcd5b6c3633a928e73e546723435b40aa8086afa
alex lee and liam stabeno <liam@>
2017-02-15 12:01:58 -0800

    add subtract, multiply and divide

--- js/scripts.js
+++ js/scripts.js
@@ -14,35 +14,33 @@ var divide = function(number1, number2){
   return number1 / number2;
 };
 
-
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-
-//alert(add(number1, number2));
-//alert(subtract(number1, number2));
-//alert(multiply(number1, number2));
-alert(divide(number1, number2));
-
-/*
-var weight = parseInt(prompt("What is your weight in kilograms?"));
-var height = parseInt(prompt("What is your height in meters?"));
-var calculateBMI = function(weight,height){
-	return(weight / height / height);
-};
-alert("Your BMI is " + calculateBMI(weight,height) + ". Go to the hospital."); */
-
-
-/*
-var fahrenheit = parseInt(prompt("Enter fahrenheit: "));
-var celsius = parseInt(prompt("Enter celcius: "));
-
-var celsiusToFahrenheit = function(cel){
-	return (cel*(9/5)) +32;
-}
-
-var fahrenheitToCelsius = function(fah){
-	return (fah-32) * (5/9);
-};
-
-alert("Celsius to fahrenheit is: " + celsiusToFahrenheit(celsius));
-alert("Fahrenheit to celsius is: " + fahrenheitToCelsius(fahrenheit)); */
+$(function(){
+  $("form#addForm").submit(function(){
+    event.preventDefault();
+    var num1 = parseInt($("#add1").val());
+    var num2 = parseInt($("#add2").val());
+    var answer = add(num1,num2);
+    $(".output").text(answer);
+  });
+  $("form#subtractForm").submit(function(){
+    event.preventDefault();
+    var num1 = parseInt($("#subtract1").val());
+    var num2 = parseInt($("#subtract2").val());
+    var answer = subtract(num1,num2);
+    $(".output").text(answer);
+  });
+  $("form#multiplyForm").submit(function(){
+    event.preventDefault();
+    var num1 = parseInt($("#multiply1").val());
+    var num2 = parseInt($("#multiply2").val());
+    var answer = multiply(num1,num2);
+    $(".output").text(answer);
+  });
+  $("form#divideForm").submit(function(){
+    event.preventDefault();
+    var num1 = parseInt($("#divide1").val());
+    var num2 = parseInt($("#divide2").val());
+    var answer = divide(num1,num2);
+    $(".output").text(answer);
+  });
+});
